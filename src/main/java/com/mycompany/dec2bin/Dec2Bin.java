9bc765fb16f44ead1de4990bcb3fa437fcdcb8e3
laura <laura@laura-Victus-by-HP-Laptop-16-d0xxx>
2022-03-23 12:16:55 +0100

    Copiada clase DecBin

---
+++ src/main/java/com/mycompany/dec2bin/Dec2Bin.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.dec2bin;
+
+/**
+ *
+ * @author laura
+ */
+public class Dec2Bin{
+    public static void main(String args[]){
+       int decimal = 252222;
+       String binario = "";
+       while ( decimal > 0 ) {
+         binario = decimal % 2 + binario;
+         decimal /= 2;
+       }
+       System.out.println(binario);   
+    }
+ }
